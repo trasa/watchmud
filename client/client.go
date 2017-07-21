101d923103890ea3a504b7caf005541f1e7999c9
Tony Rasa <trasa@meancat.com>
2017-07-21 10:53:33 -0700

    client.Client is ok (slight stutter...)

--- client/client.go
+++ client/client.go
@@ -2,6 +2,7 @@ package client
 
 import "github.com/trasa/watchmud/player"
 
+//noinspection GoNameStartsWithPackageName
 type Client interface {
 	Send(message interface{}) // todo return err
 	SetPlayer(player player.Player)
