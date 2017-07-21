817793c3059b89c93a749421003cdefc06ef59c3
Tony Rasa <trasa@meancat.com>
2017-07-21 16:58:46 -0700

    translate line - TODO

--- web/translator.go
+++ web/translator.go
@@ -1,9 +1,19 @@
 package web
 
 import (
+	"errors"
 	"github.com/trasa/watchmud/message"
 )
 
+func translateLineToRequest(line string) (request message.Request, err error) {
+	// have an input string like
+	// tell bob hi there
+	// turn into Request = message.TellRequest { "bob", "hi there" }
+	// and so on.
+	// difficulty: don't repeat yourself with the translateToRequest below that turns a map
+	// into an object ... so should this return a map or jump directly to Request?
+	return nil, errors.New("not implemented")
+}
 func translateToRequest(body map[string]string) (request message.Request, err error) {
 	err = nil
 	switch body["msg_type"] {
