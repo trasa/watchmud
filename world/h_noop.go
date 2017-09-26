57a109a11fb436493cbf3befe786ab77cbfcc569
Tony Rasa <trasa@meancat.com>
2017-09-26 16:05:07 -0700

    settings and no op handler

---
+++ world/h_noop.go
@@ -0,0 +1,17 @@
+package world
+
+import (
+	"github.com/trasa/watchmud/message"
+	"log"
+)
+
+
+func (w *World) handleNoOp(msg *message.IncomingMessage) {
+	//noinspection ALL
+	if VERBOSE_LOGGING {
+		log.Printf("Player %s sent a No Op Request\n", msg.Player.GetName())
+	}
+	msg.Player.Send(message.NoOpResponse{
+		Response: message.NewSuccessfulResponse("no_op"),
+	})
+}
---
+++ world/settings.go
@@ -0,0 +1,6 @@
+package world
+
+// where we're putting constants and settings and stuff,
+// until we get around to building something better.
+
+const VERBOSE_LOGGING = false
