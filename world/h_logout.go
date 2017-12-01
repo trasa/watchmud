d733548dd8736fb7b0edd0013cb3fa8587a54919
Tony Rasa <trasa@meancat.com>
2017-11-30 21:13:18 -0800

    LogoutNotification message
    fixes #3

--- world/h_logout.go
+++ world/h_logout.go
@@ -3,11 +3,20 @@ package world
 import (
 	"github.com/trasa/watchmud/gameserver"
 	"log"
+	"github.com/trasa/watchmud/message"
 )
 
 func (w *World) handleLogout(msg *gameserver.HandlerParameter) {
 	if msg.Player != nil {
 		log.Printf("Player %s Logout", msg.Player.GetName())
+		playerRoom := w.getRoomContainingPlayer(msg.Player)
 		w.RemovePlayer(msg.Player)
+		if playerRoom != nil {
+			playerRoom.Send(message.LogoutNotification{
+				Success: true,
+				ResultCode: "OK",
+				PlayerName: msg.Player.GetName(),
+			})
+		}
 	}
 }
