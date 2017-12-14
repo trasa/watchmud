60333a56c4fcdd21e3b7ef5264bde1a828cd07dd
Tony Rasa <trasa@meancat.com>
2017-12-13 16:46:22 -0800

    mock zone list

---
+++ web/builder/src/app/mock-zones.ts
@@ -0,0 +1,6 @@
+import { Zone } from './zone'
+
+export const ZONES: Zone[] = [
+  { id: "void", name: "The Void", resetMode: 0, lifetimeMinutes: 0, enabled: true},
+  { id: "sample", name: "Sample Zone", resetMode: 2, lifetimeMinutes: 3, enabled: true}
+];
--- web/builder/src/app/zones/zones.component.ts
+++ web/builder/src/app/zones/zones.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Zone } from '../zone';
+import { ZONES } from '../mock-zones';
 
 @Component({
   selector: 'app-zones',
@@ -7,13 +8,13 @@ import { Zone } from '../zone';
   styleUrls: ['./zones.component.css']
 })
 export class ZonesComponent implements OnInit {
-  zone: Zone = {
-    id: 'sample',
-    name: 'Sample Zone',
-    resetMode: 2,
-    lifetimeMinutes: 3,
-    enabled: true
-  };
+  zones = ZONES;
+
+  selectedZone: Zone;
+
+  onSelect(zone: Zone): void {
+    this.selectedZone = zone;
+  }
 
 
   constructor() { }
