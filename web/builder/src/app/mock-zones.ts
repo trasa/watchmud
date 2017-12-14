7ca5d53de8b4e16c44ea068228642b5cdaad8313
Tony Rasa <trasa@meancat.com>
2017-12-14 13:41:22 -0800

    removing builder and old ng new app

--- web/builder/src/app/mock-zones.ts
+++
@@ -1,6 +0,0 @@
-import { Zone } from './zone'
-
-export const ZONES: Zone[] = [
-  { id: "void", name: "The Void", resetMode: 0, lifetimeMinutes: 0, enabled: true},
-  { id: "sample", name: "Sample Zone", resetMode: 2, lifetimeMinutes: 3, enabled: true}
-];
